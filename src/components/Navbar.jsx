8986093d836cd1f63bbefcb8bb3b312eb262df2a
dhenmasadhi <dhenmasa@gmail.com>
2024-05-16 18:26:37 +0700

    memindahkan logo ke asset

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -20,11 +21,7 @@ const Navbar = () => {
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              <img
-                src="/public/logo.png"
-                alt="Logo"
-                className="w-[175px] h-full"
-              />
+              <img src={logo} alt="Logo" className="w-[175px] h-full" />
             </span>
           </a>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
