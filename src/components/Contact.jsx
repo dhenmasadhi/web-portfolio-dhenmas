d4b2a312fceed7357287ba05e27191b41a1222a3
dhenmasadhi <dhenmasa@gmail.com>
2024-07-02 10:35:34 +0700

    fix contact me

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -109,7 +109,7 @@ const Contact = () => {
               htmlFor="message"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              Your Massage
+              Your Message
             </label>
           </motion.div>
 
